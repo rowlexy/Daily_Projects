8470a1ad62dc2870f87daf74c3ef3e187524c74d
Rowland <rowlandelegbede@gmail.com>
2025-09-05 21:36:30 +0200

    sending attachment via email automation

--- 24_email_script.py
+++ 24_email_script.py
@@ -2,8 +2,11 @@ from dotenv import load_dotenv
 import os, smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-load_dotenv()
+from email import encoders # for attachments
+from email.mime.base import MIMEBase # for attachments
+from pathlib import Path
 
+load_dotenv()
 sender = os.getenv('EMAIL')
 password = os.getenv('PASSWORD')
 recipients = os.getenv('RECIPIENT')
@@ -16,7 +19,7 @@ distribution_list = []
 for contact in contact_list:
     distribution_list.append(contact.strip())
 
-def send_email(subject, mail_list, body):
+def send_email(subject, mail_list, body, attachment):
     try:
         message = MIMEMultipart()
         message['From'] = sender
@@ -24,7 +27,18 @@ def send_email(subject, mail_list, body):
         message['Subject'] = subject
         content = MIMEText(body, 'plain')
         message.attach(content)
-    
+
+        # Add attachment here
+        if attachment and Path(attachment).exists():
+            filename = Path(attachment).name
+            with open(attachment, 'rb') as file:
+                part = MIMEBase('application', 'pdf')
+                part.set_payload(file.read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', f'attachment: filename={filename}')
+            message.attach(part)
+        else:
+            print(f'File not found: {attachment}')
         with smtplib.SMTP(SMTP_LINK, SMTP_PORT) as server:
             server.starttls()
             server.login(sender, password)
@@ -34,7 +48,8 @@ def send_email(subject, mail_list, body):
         print(f'Email not sent: {e}')
          
 if __name__ == '__main__':
-    body = "This is another day of sending email via automation"
+    body = "This is another day of sending email via automation\nPassword to pdf file is Rowzay_27"
     mail_list = distribution_list
     subject = 'Email Automation'
-    send_email(subject, mail_list, body)
+    attachment = 'encrypted_testfile.pdf'
+    send_email(subject, mail_list, body, attachment)
